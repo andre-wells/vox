e1e2c3d4d8dee7d5892802ce6f3cd4047c1085b7
Andre Wells <Andre.Wells@korbitecinc.com>
2021-12-21 22:23:01 +0200

    Initial addition of Question 2

---
+++ Question2/GetCountryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    internal class GetCountryHandler
+    {
+        const string Key = "AIzaSyAOT2CYIlZW0nifJQpZiJ-rIRNGX1YnhPw";
+
+        public GetCountryHandler(HttpClient httpClient)
+        {
+
+        }
+
+        // 
+        public async Task<string> GetCountryAsync(double latitude, double longitude)
+        {
+            Console.WriteLine("hi");
+
+            return null;
+        }
+    }
+}
---
+++ Question2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Question2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args != null 
+                && args.Length == 2 
+                && double.TryParse(args[0], NumberStyles.Any, CultureInfo.InvariantCulture, out var lat) 
+                && double.TryParse(args[1], NumberStyles.Any, CultureInfo.InvariantCulture, out var lon))
+            {
+                var sp = new ServiceCollection()
+                    .AddHttpClient()
+                    .AddTransient<GetCountryHandler>()
+                    .BuildServiceProvider();
+
+                await sp.GetRequiredService<GetCountryHandler>().GetCountryAsync(1, 2);
+            }
+            else
+                Console.WriteLine("Invalid arguments.");
+        }
+
+    }
+}
