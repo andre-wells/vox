ad5c127fbbe6eb1564085d8370391e0fa9f795ea
Andre Wells <Andre.Wells@korbitecinc.com>
2021-12-23 12:25:11 +0200

    Added initial unit tests

--- Question3a/Q3aTests/InvoicePageTests.cs
+++ Question3a/Q3aTests/InvoicePageTests.cs
@@ -46,5 +46,54 @@ namespace Q3aTests
             component.Find("#amount-received-input").Change("70");
             component.Find("#distribute-amount-button").Click();
         }
+
+        [Fact]
+        public void OnInitializedAsync_FakeDataRetrieved_DisplaysInvoices()
+        {
+            var fakeInvoiceService = new FakeInvoiceService();
+
+            Services.AddSingleton<IInvoiceService>(fakeInvoiceService);
+
+            Invoice[] invoices = {
+                new Invoice {
+                    Id = 1,
+                    Date = new DateTime(2021, 11, 23),
+                    TotalAmount = 200,
+                    AmountPaid = 50,
+                    Balance = 150
+                },
+                new Invoice {
+                    Id = 2,
+                    Date = new DateTime(2021, 11, 24),
+                    TotalAmount = 500,
+                    AmountPaid = 200,
+                    Balance = 300
+                },
+                new Invoice {
+                    Id = 3,
+                    Date = new DateTime(2021, 11, 25),
+                    TotalAmount = 300,
+                    AmountPaid = 25,
+                    Balance = 275
+                }
+            };
+
+            fakeInvoiceService.setInvoicesToReturn(invoices);
+
+            var component = RenderComponent<Invoices>();
+
+            foreach (var item in invoices)
+            {
+                component.Find($"#invoice-{item.Id}-total").MarkupMatches(
+                $"<td id=\"invoice-{item.Id}-total\">{item.TotalAmount}</td>");
+
+                component.Find($"#invoice-{item.Id}-paid").MarkupMatches(
+                $"<td id=\"invoice-{item.Id}-paid\">{item.AmountPaid}</td>");
+
+                component.Find($"#invoice-{item.Id}-balance").MarkupMatches(
+                $"<td id=\"invoice-{item.Id}-balance\">{item.Balance}</td>");
+
+            }            
+        }
     }
 }
